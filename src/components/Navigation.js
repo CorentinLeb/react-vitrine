d7094b40812c60810a566ea3734ad89d2b4befbe
Corentin LE BERRE <leberre.corentin@gmail.com>
2021-04-01 15:22:15 +0200

    setting components

---
+++ src/components/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+    return (
+        <div className="navigation">
+            <ul>
+                <NavLink to="/" exact className="hover" activeClassName="nav-active">
+                    <li>accueil</li>
+                </NavLink>
+            </ul>
+        </div>
+    );
+};
+
+export default Navigation;
