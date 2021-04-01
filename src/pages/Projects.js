1ce3ce613b628463441348332397131f4aae3c61
Corentin LE BERRE <leberre.corentin@gmail.com>
2021-04-01 16:48:06 +0200

    last commit of the day, react project doing fine

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import ButtonsBottom from '../components/ButtonsBottom';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+import Project from '../components/Project';
 
 export const Project1 = () => {
     return (
         <main>
             <div className="project">
-                Project 1
+                <Navigation />
+                <Logo />
+                <Project  projectNumber={0} />
+                <ButtonsBottom  left={'/'} right={'/project-2'} />
             </div>
         </main>
     );
@@ -14,7 +21,10 @@ export const Project2 = () => {
     return (
         <main>
             <div className="project">
-                Project 2
+                <Navigation />
+                <Logo />
+                <Project  projectNumber={1} />
+                <ButtonsBottom  left={'/project-1'} right={'/project-3'} />
             </div>
         </main>
     );
@@ -23,7 +33,10 @@ export const Project3 = () => {
     return (
         <main>
             <div className="project">
-                Project 3
+                <Navigation />
+                <Logo />
+                <Project  projectNumber={2} />
+                <ButtonsBottom  left={'/project-2'} right={'/project-4'} />
             </div>
         </main>
     );
@@ -32,7 +45,10 @@ export const Project4 = () => {
     return (
         <main>
             <div className="project">
-                Project 4
+                <Navigation />
+                <Logo />
+                <Project  projectNumber={3} />
+                <ButtonsBottom  left={'/project-3'} right={'/'} />
             </div>
         </main>
     );
